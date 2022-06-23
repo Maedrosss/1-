08c225c01fc559692d6d1354a80a353c71c70f8d
Maedrosss <100582040+Maedrosss@users.noreply.github.com>
2022-06-24 01:13:55 +0300

    Add files via upload

---
+++ Java_1.js
@@ -0,0 +1,12 @@
+'привет, Java!'
+var myThing;
+myThing=5;
+mything='это строка'
+var numberNine=9;
+var stringNine="9";
+numberNine+numberNine
+stringNine+stringNine
+var greeting="привет, ";
+var myname="Ник";
+greeting+myname;
+myname.length;
